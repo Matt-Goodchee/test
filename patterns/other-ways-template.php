d28bc8ee6eddafb88c9f5ca334daf1bdd0c4de3c
Matt <matt@goodchee.com>
2023-05-30 11:26:30 -0700

    dev

---
+++ patterns/other-ways-template.php
@@ -0,0 +1,81 @@
+<?php
+/**
+* Title: Other Ways To Go Template
+* Slug: rc/other-ways-template
+* Description: Example other ways to go page template
+* Categories: rc
+*/
+?>
+<!-- wp:kadence/rowlayout {"uniqueID":"_616098-87","columns":1,"customRowGutter":[30,"",""],"colLayout":"equal","tabletPadding":["","md","","md"],"inheritMaxWidth":true,"padding":["md","xxl","0","xxl"],"kbVersion":2,"className":"hero-text-row"} -->
+<!-- wp:kadence/column {"borderWidth":["","","",""],"uniqueID":"_5c96cb-e1","borderStyle":[{"top":["","",""],"right":["","",""],"bottom":["","",""],"left":["","",""],"unit":"px"}]} -->
+<div class="wp-block-kadence-column kadence-column_5c96cb-e1"><div class="kt-inside-inner-col"><!-- wp:heading {"textAlign":"center","level":1} -->
+<h1 class="wp-block-heading has-text-align-center">Other Ways To Go</h1>
+<!-- /wp:heading --></div></div>
+<!-- /wp:kadence/column -->
+<!-- /wp:kadence/rowlayout -->
+
+<!-- wp:kadence/rowlayout {"uniqueID":"_9ed2ba-9c","columns":3,"collapseGutter":"wider","customRowGutter":[60,"",""],"columnGutter":"wider","customGutter":[64,"",""],"colLayout":"right-half","align":"full","tabletPadding":["xs","md","","md"],"inheritMaxWidth":true,"padding":["lg","xxl","xl","xxl"],"mobilePadding":["","","lg",""],"kbVersion":2,"className":"other-ways-row"} -->
+<!-- wp:kadence/column {"borderWidth":["","","",""],"uniqueID":"_81e3fd-37","borderStyle":[{"top":["","",""],"right":["","",""],"bottom":["","",""],"left":["","",""],"unit":"px"}],"className":"col-menu"} -->
+<div class="wp-block-kadence-column kadence-column_81e3fd-37 col-menu"><div class="kt-inside-inner-col"><!-- wp:shortcode -->
+[other_ways_menu]
+<!-- /wp:shortcode --></div></div>
+<!-- /wp:kadence/column -->
+
+<!-- wp:kadence/column {"id":3,"borderWidth":["","","",""],"uniqueID":"_b20f1d-2f","borderStyle":[{"top":["","",""],"right":["","",""],"bottom":["","",""],"left":["","",""],"unit":"px"}],"className":"col-image"} -->
+<div class="wp-block-kadence-column kadence-column_b20f1d-2f col-image"><div class="kt-inside-inner-col"><!-- wp:acf/image-border {"name":"acf/image-border","data":{"image_border_group_border":"bottom-left","_image_border_group_border":"field_image_border_image_border_group_border","image_border_group_rounded":"bottom-right","_image_border_group_rounded":"field_image_border_image_border_group_rounded","image_border_group":"","_image_border_group":"field_image_border_image_border_group"},"mode":"preview"} -->
+<!-- wp:image {"id":110,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="http://localhost/rc/wp-content/uploads/2023/05/iStock-458463141-1-809x1024.jpg" alt="" class="wp-image-110"/></figure>
+<!-- /wp:image -->
+<!-- /wp:acf/image-border --></div></div>
+<!-- /wp:kadence/column -->
+
+<!-- wp:kadence/column {"id":2,"borderWidth":["","","",""],"uniqueID":"_30aed8-17","borderStyle":[{"top":["","",""],"right":["","",""],"bottom":["","",""],"left":["","",""],"unit":"px"}],"className":"col-text"} -->
+<div class="wp-block-kadence-column kadence-column_30aed8-17 col-text"><div class="kt-inside-inner-col"><!-- wp:heading {"placeholder":"Heading"} -->
+<h2 class="wp-block-heading">MAX Light Rail</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"placeholder":"Paragraph text","textColor":"dark-brown","fontSize":"x-large"} -->
+<p class="has-dark-brown-color has-text-color has-x-large-font-size">15-minute service throughout the day.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:kadence/spacer {"hAlign":"left","spacerHeight":20,"dividerColor":"#E3B505","dividerWidth":100,"dividerWidthUnits":"px","dividerHeight":3,"uniqueID":"_39151b-4e"} -->
+<div class="wp-block-kadence-spacer aligncenter kt-block-spacer-_39151b-4e"><div class="kt-block-spacer kt-block-spacer-halign-left"><hr class="kt-divider"/></div></div>
+<!-- /wp:kadence/spacer -->
+
+<!-- wp:paragraph -->
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"id":111,"width":391,"height":50,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full is-resized"><img src="http://localhost/rc/wp-content/uploads/2023/05/trimet.png" alt="" class="wp-image-111" width="391" height="50"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph -->
+<p>Information on how to connect or purchase tickets for this provider. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><strong>Servicing the Portland, Oregon region with stops at:</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list -->
+<ul><!-- wp:list-item -->
+<li>Clackamas Town Center</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Milwaukie</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Gresham</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list -->
+
+<!-- wp:kadence/iconlist {"listStyles":[{"size":["","",""],"sizeType":"px","lineHeight":["","",""],"lineType":"px","letterSpacing":"","family":"","google":false,"style":"","weight":"","variant":"","subset":"","loadGoogle":true,"color":"#F0421C","textTransform":""}],"listLabelGap":0,"uniqueID":"_01e5e0-e4","listMargin":["0","0","xs","0"],"iconSize":[18,"",""],"icon":"fe_arrowRight","color":"#F0421C","className":"icon-link"} -->
+<div class="wp-block-kadence-iconlist kt-svg-icon-list-items kt-svg-icon-list-items_01e5e0-e4 kt-svg-icon-list-columns-1 alignnone icon-link"><ul class="kt-svg-icon-list"><!-- wp:kadence/listitem {"uniqueID":"_fb6541-fc","text":"\u003ca href=\u0022#\u0022\u003ePurchase Fare on TriMet Website\u003c/a\u003e"} -->
+<li class="wp-block-kadence-listitem kt-svg-icon-list-item-wrap kt-svg-icon-list-item-_fb6541-fc"><span data-name="USE_PARENT_DEFAULT_ICON" data-class="kt-svg-icon-list-single" class="kadence-dynamic-icon"></span><span class="kt-svg-icon-list-text"><a href="#">Purchase Fare on TriMet Website</a></span></li>
+<!-- /wp:kadence/listitem --></ul></div>
+<!-- /wp:kadence/iconlist --></div></div>
+<!-- /wp:kadence/column -->
+<!-- /wp:kadence/rowlayout -->
