750e3dc59aa657cc9f9685de81d78e5bdd9ef819
Matt <matt@goodchee.com>
2023-06-13 18:00:14 -0700

    qa

--- assets/js/src/main.js
+++ assets/js/src/main.js
@@ -1,38 +1,45 @@
-/* CUSTOM
+/* Google Translate
 ========================================================= */
+window.googleTranslateElementInit = function () {
+  new google.translate.TranslateElement(
+    {
+      pageLanguage: "en",
+      includedLanguages: "es,ar,en,ru,ko,th,vi,zh-CN",
+      layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+    },
+    "google_translate_element"
+  );
+};
 
 /* GSAP 
 ========================================================= */
 
-// Config 
-
+// Config
 gsap.registerPlugin(ScrollTrigger);
 
 gsap.config({
-  nullTargetWarn: false
+  nullTargetWarn: false,
 });
 
 // InView
-
 var inViewElements = gsap.utils.toArray(".scrolled");
 inViewElements.forEach(function (inviewElement) {
   ScrollTrigger.create({
     trigger: inviewElement,
     start: "top 50%",
     onToggle: () => {
-      inviewElement.classList.add('inview');
-    }
+      inviewElement.classList.add("inview");
+    },
   });
 });
 
-// Animate Single Element 
-
+// Animate Single Element
 var animateElement = gsap.utils.toArray(".animated");
 animateElement.forEach(function (element) {
-  var positionValueX = element.getAttribute('data-anim-x') || 0;
-  var positionValueY = element.getAttribute('data-anim-y') || 0;
-  var scaleValue = element.getAttribute('data-anim-scale') || 1;
-  var delayValue = element.getAttribute('data-anim-delay') || 0;
+  var positionValueX = element.getAttribute("data-anim-x") || 0;
+  var positionValueY = element.getAttribute("data-anim-y") || 0;
+  var scaleValue = element.getAttribute("data-anim-scale") || 1;
+  var delayValue = element.getAttribute("data-anim-delay") || 0;
   gsap.set(element, { opacity: 1 });
   gsap.from(element, {
     opacity: 0,
@@ -42,7 +49,7 @@ animateElement.forEach(function (element) {
     ease: "power3.inOut",
     delay: delayValue,
     scale: scaleValue,
-    transformOrigin: 'center center',
+    transformOrigin: "center center",
     scrollTrigger: {
       trigger: element,
       start: "top 70%",
@@ -51,7 +58,6 @@ animateElement.forEach(function (element) {
 });
 
 // Animate Grouped Elements
-
 var staggerGroup = gsap.utils.toArray(".animated-group");
 staggerGroup.forEach(function (group) {
   var elements = group.querySelectorAll(".animated-entry");
@@ -65,58 +71,73 @@ staggerGroup.forEach(function (group) {
     opacity: 0,
     duration: 1,
     ease: "power3.inOut",
-    stagger: 0.10,
+    stagger: 0.1,
   });
 });
 
 // Route SVG
-
 const routeTL = gsap.timeline({});
-routeTL.to('.route-map-svg', { opacity: 1 })
-  .fromTo('.route-map-svg .route-logos g', {
-    opacity: 0,
-    scale: 0.95,
-  }, {
-    opacity: 1,
-    scale: 1,
-    stagger: 0.2,
-    delay: 0.5,
-    duration: 1
-  })
-  .fromTo('.route-map-svg .main-routes g', {
-    opacity: 0,
-    scale: 0.98,
-  }, {
-    opacity: 1,
-    scale: 1,
-    stagger: 0.2,
-    delay: 0.5,
-    duration: 1
-  }, "<")
-  .fromTo('.route-map-svg #city-names-and-background', {
-    opacity: 0,
-  }, {
-    opacity: 1,
-    duration: 1
-  }, ">-0.8")
-  ;
+routeTL
+  .to(".route-map-svg", { opacity: 1 })
+  .fromTo(
+    ".route-map-svg .route-logos g",
+    {
+      opacity: 0,
+      scale: 0.95,
+    },
+    {
+      opacity: 1,
+      scale: 1,
+      stagger: 0.2,
+      delay: 0.5,
+      duration: 1,
+    }
+  )
+  .fromTo(
+    ".route-map-svg .main-routes g",
+    {
+      opacity: 0,
+      scale: 0.98,
+    },
+    {
+      opacity: 1,
+      scale: 1,
+      stagger: 0.2,
+      delay: 0.5,
+      duration: 1,
+    },
+    "<"
+  )
+  .fromTo(
+    ".route-map-svg #city-names-and-background",
+    {
+      opacity: 0,
+    },
+    {
+      opacity: 1,
+      duration: 1,
+    },
+    ">-0.8"
+  );
 
 // Logo Animation
-
 const logoTL = gsap.timeline({});
-logoTL.to('.main-logo', { opacity: 1 })
-  .fromTo('.main-logo g', {
+logoTL.to(".main-logo", { opacity: 1 }).fromTo(
+  ".main-logo g",
+  {
     opacity: 0,
-    x: -20
-  }, {
+    x: -20,
+  },
+  {
     opacity: 1,
     x: 0,
     stagger: 0.15,
-    duration: 0.8
-  });
+    duration: 0.8,
+  }
+);
 
-
-// jQuery Doc Ready
+/* jQuery Doc Ready
+========================================================= */
 (function ($) {
   // Mobile Menu
   let menuButton = $("#navbar-toggle");
@@ -124,37 +145,48 @@ logoTL.to('.main-logo', { opacity: 1 })
   if (menuButton.length && menu.length) {
     function openMenu() {
       gsap.set(menu, { display: "block", y: 0, scale: 1 });
-      gsap.fromTo(menu, {
-        autoAlpha: 0,
-        x: 20
-      }, {
-        autoAlpha: 1,
-        x: 0,
-        duration: 0.3
-      });
-      gsap.fromTo("#mobile-nav figure", {
-        autoAlpha: 0,
-        x: 50
-      }, {
-        autoAlpha: 1,
-        x: 0,
-        duration: 0.3
-      });
+      gsap.fromTo(
+        menu,
+        {
+          autoAlpha: 0,
+          x: 20,
+        },
+        {
+          autoAlpha: 1,
+          x: 0,
+          duration: 0.3,
+        }
+      );
+      gsap.fromTo(
+        "#mobile-nav figure",
+        {
+          autoAlpha: 0,
+          x: 50,
+        },
+        {
+          autoAlpha: 1,
+          x: 0,
+          duration: 0.3,
+        }
+      );
       menu.addClass("open");
       menuButton.addClass("open");
     }
     function closeMenu() {
-      gsap.fromTo(menu, {
-        autoAlpha: 1,
-        x: 0
-      }, {
-        autoAlpha: 0,
-        x: 10,
-        duration: 0.15,
-        onComplete: () => {
-          gsap.set(menu, { display: "none" });
+      gsap.fromTo(
+        menu,
+        {
+          autoAlpha: 1,
+          x: 0,
+        },
+        {
+          autoAlpha: 0,
+          x: 10,
+          duration: 0.15,
+          onComplete: () => {
+            gsap.set(menu, { display: "none" });
+          },
         }
-      },
       );
       menu.removeClass("open");
       menuButton.removeClass("open");
@@ -211,7 +243,6 @@ logoTL.to('.main-logo', { opacity: 1 })
 
   // Mini Provider Grid
   let slides = $(".providers-mini .wrapper");
-  console.log(slides);
   if (slides.length > 0) {
     function initSlickSlider() {
       slides.slick({
@@ -256,5 +287,20 @@ logoTL.to('.main-logo', { opacity: 1 })
     checkViewportWidth();
   }
 
-
+  // Move GTranslate Widget
+  function translateWidget() {
+    const widget = $("#gtranslate");
+    if (widget.length) {
+      if (window.matchMedia("(max-width: 768px)").matches) {
+        widget.appendTo($("#mobile-nav .search-form"));
+      } else {
+        widget.prependTo($(".mobile-nav-header .menu-items"));
+      }
+    }
+  }
+  $(window).resize(
+    debounce(function () {
+      translateWidget();
+    }, 250)
+  );
 })(jQuery);
